91ac3e054f5e5560a7cca080a0fba0c561a88643
mattn <mattn.jp@gmail.com>
2014-02-18 18:49:26 +0900

    Fix Search

--- jmardb.go
+++ jmardb.go
@@ -7,12 +7,19 @@ import (
 	"net/url"
 )
 
+type Search struct {
+	Datetime	string `json:"datetime"`
+	Headline	[]string `json:"headline"`
+	Link		string `json:"link"`
+	Title		string `json:"title"`
+}
+
 type Forecast struct {
 	Code string `json:"code"`
 	Name string `json:"name"`
 }
 
-type Data struct {
+type Area struct {
 	JISX0402  string     `json:"jisx0402"`
 	Longitude float64    `json:"longitude"`
 	Latitude  float64    `json:"latitude"`
@@ -27,7 +34,7 @@ func NewClient() *client {
 	return &client{}
 }
 
-func (c *client) Search(status string, infotype string, title string, headtitle string, eventid string, areaname string, areacode string, datetime string, reportdatetime string, targetdatetime string, validdatetime string, limit int, offset int, order string) ([]Data, error) {
+func (c *client) Search(status string, infotype string, title string, headtitle string, eventid string, areaname string, areacode string, datetime string, reportdatetime string, targetdatetime string, validdatetime string, limit int, offset int, order string) ([]Search, error) {
 	param := url.Values{}
 	param.Set("status", status)
 	param.Set("infotype", infotype)
@@ -49,18 +56,16 @@ func (c *client) Search(status string, infotype string, title string, headtitle 
 	}
 	defer res.Body.Close()
 	var datas struct {
-		Data []Data `json:"data"`
+		Data []Search `json:"data"`
 	}
 	err = json.NewDecoder(res.Body).Decode(&datas)
 	if err != nil {
 		return nil, err
 	}
 	return datas.Data, nil
-
-
 }
 
-func (c *client) Area(latitude, longitude float64, limit int) ([]Data, error) {
+func (c *client) Area(latitude, longitude float64, limit int) ([]Area, error) {
 	param := url.Values{}
 	param.Set("latitude", fmt.Sprint(latitude))
 	param.Set("longitude", fmt.Sprint(longitude))
@@ -73,7 +78,7 @@ func (c *client) Area(latitude, longitude float64, limit int) ([]Data, error) {
 	}
 	defer res.Body.Close()
 	var datas struct {
-		Data []Data `json:"data"`
+		Data []Area `json:"data"`
 	}
 	err = json.NewDecoder(res.Body).Decode(&datas)
 	if err != nil {
